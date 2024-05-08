ca26c2c2e7de7279000015fd9e991a30526513ac
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2024-05-08 21:56:39 +0200

    wip

--- src/project.v
+++ src/project.v
@@ -10,9 +10,9 @@
 module tt_um_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    //input  wire [7:0] uio_in,   // IOs: Input path
-    //output wire [7:0] uio_out,  // IOs: Output path
-    //output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
@@ -24,5 +24,8 @@ module tt_um_example (
     .reset(~rst_n),
     .clock(clk)
   );
+
+  assign uio_out = 0;
+  assign uio_oe  = 0;
 
 endmodule
