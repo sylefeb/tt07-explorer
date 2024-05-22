82c3ecb4172c44885f76b418561d9235c7f78219
Sylvain Lefebvre <Sylvain.Lefebvre@inria.fr>
2024-05-22 06:48:26 +0200

    redesign

--- src/build.v
+++ src/build.v
@@ -213,7 +213,9 @@ module sb_io_inout #(parameter TYPE=6'b1101_00) (
 	input        oe,
   input        out,
 	output       in,
-  inout        pin
+  input        pin_i,
+  output       pin_o,
+  output       pin_oe
   );
 
   wire unused;
@@ -225,7 +227,9 @@ module sb_io_inout #(parameter TYPE=6'b1101_00) (
 `endif
     .PIN_TYPE(TYPE)
   ) sbio (
-      .PACKAGE_PIN(pin),
+      .PACKAGE_PIN_I(pin_i),
+      .PACKAGE_PIN_O(pin_o),
+      .PACKAGE_PIN_OE(pin_oe),
 			.OUTPUT_ENABLE(oe),
       .D_OUT_0(out),
       .D_OUT_1(out),
